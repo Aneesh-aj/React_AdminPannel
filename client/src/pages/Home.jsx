40448e36c75044b54dd01fdcd6b5357c9fec89d3
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2023-12-21 22:07:37 +0530

    new commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
         </p>
         <p className="text-gray-600 mb-6">
           Curious about our latest updates? Check out our blog or navigate
-          through the exciting features we offer.
+          through the exciting features we offer. explore more in this website
         </p>
       </div>
     </div>
