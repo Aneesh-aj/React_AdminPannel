988658546e8a1487d5be5199854b344d5683bfbf
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2023-12-26 09:16:01 +0530

    error page cleared

--- api/utils/error.js
+++ api/utils/error.js
@@ -2,7 +2,6 @@ export const errorHandler = (statusCode, message) => {
     const error = new Error();
     error.statusCode = statusCode;
     error.message = message;
-    console.log("error hadler ")
-    console.log("eht erorr",error.message)
+  
     return error;
 }
