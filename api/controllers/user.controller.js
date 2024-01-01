28c9ef677754562a55053030ba8bb4c22078b34e
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2024-01-01 20:08:32 +0530

    changes added

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,7 +3,6 @@ import bcryptjs from "bcryptjs";
 import User from "../models/user.js";
 
 
-
 export const updateUser = async (req, res, next) => {
   if (req.user.id != req.params.id) {
     return next(errorHandler(401, "You can update only your account!"));
